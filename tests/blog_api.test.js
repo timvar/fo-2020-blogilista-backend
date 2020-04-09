fcf57cf10a712b6f8364e20b931117ff8cfcaacd
Timo Varila <timo.varila@siili.com>
2020-04-09 13:41:51 +0300

    4.8 step1

---
+++ tests/blog_api.test.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const { sixBlogList } = require('./testblogs')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blog = new Blog(sixBlogList[0])
+  await blog.save()
+  blog = new Blog(sixBlogList[1])
+  await blog.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are two blogs', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(2)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
